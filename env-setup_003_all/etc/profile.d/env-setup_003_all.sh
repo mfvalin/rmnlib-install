6226f50408c8d7106f2de49a624edb947881eb76
Michel Valin <mfvalin@gmail.com>
2018-02-17 13:49:10 -0500

    added setup  bootstrap

---
+++ env-setup_003_all/etc/profile.d/env-setup_003_all.sh
@@ -0,0 +1,10 @@
+export ENV_RELEASE=${ENV_RELEASE:-003}
+[[ -d ${SSM_DOMAIN_HOME%/*}/base_${ENV_RELEASE} ]] && . ssmuse-sh -d ${SSM_DOMAIN_HOME%/*}/base_${ENV_RELEASE}
+[[ -d ${SSM_DOMAIN_HOME%/*}/env_${ENV_RELEASE} ]] && . ssmuse-sh -d ${SSM_DOMAIN_HOME%/*}/env_${ENV_RELEASE}
+#
+. .profile_00000
+export HOME_ECssm=$(s.unified_setup-cfg -base)
+HOME_ECssm=${HOME_ECssm%/dot-profile-setup*}
+s.which local_shortcuts.dot 2>/dev/null 1>/dev/null && . local_shortcuts.dot
+#
+. .profile_zzzzz
