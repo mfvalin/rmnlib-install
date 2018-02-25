6f0e4b9dd251748cd55777395d48a75c46aa0aac
Michel Valin <mfvalin@gmail.com>
2018-02-25 15:17:38 -0500

    installer fix

--- utils-rmnlib_1.0_linux26-x86-64/bin/install_rmnlib_utils.sh
+++ utils-rmnlib_1.0_linux26-x86-64/bin/install_rmnlib_utils.sh
@@ -4,6 +4,7 @@ Where=$(readlink -e $Where)
 Where=${Where%/bin/*}/Repositories
 echo Where=$Where
 cd $Where
+export USE_RECURSIVE_SHORTCUTS=yes
 for Target in ${1:-/dev/gfortran} ; do
   echo "===== COMPILING and INSTALLING for architecture = $Target ====="
   ( \
